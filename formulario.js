6ce960151e99d31c385c5bbc30ae144b0a4f852e
Fernando <ferhepko@hotmail.com>
2021-10-10 15:51:52 -0300

    Se terminó de diseñar toda la web para celulares

--- formulario.js
+++
@@ -1,5 +0,0 @@
-const nombre = document.getElementById("nombre");
-const email = document.getElementById("email");
-const comentario = document.getElementById("comentario");
-const calidad = document.getElementById("calidad");
-const notificaciones = document.getElementById("notificaciones").checked;

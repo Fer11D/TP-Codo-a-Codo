086a3d78e472d7ed9cac969086bed20e1bb1049d
Fernando <ferhepko@hotmail.com>
2021-10-10 14:01:40 -0300

    Se terminó la página about

---
+++ formulario.js
@@ -0,0 +1,5 @@
+const nombre = document.getElementById("nombre");
+const email = document.getElementById("email");
+const comentario = document.getElementById("comentario");
+const calidad = document.getElementById("calidad");
+const notificaciones = document.getElementById("notificaciones").checked;
